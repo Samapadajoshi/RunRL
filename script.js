4373cf9e296d54cb1c77f3f84e2b3d449c735600
Samapadajoshi <sampadaj630@gmail.com>
2025-04-12 14:31:01 +0530

    files changes done

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ const ctx = canvas.getContext('2d');
 ctx.strokeStyle = 'brown';
 ctx.lineWidth = 4;
 
-// Start drawing
 ctx.beginPath();
 
-// Draw a simple curved line by connecting points
 ctx.moveTo(20, 180);     // Start at bottom-left
 ctx.lineTo(70, 140);     // Move up
 ctx.lineTo(140, 100);    // Move more up
@@ -19,7 +17,7 @@ ctx.lineTo(280, 20);     // End at top-right
 // Draw the line on the canvas
 ctx.stroke();
 
-// When the button is clicked, show an alert (or redirect later)
+
 document.getElementById('bookCallBtn').addEventListener('click', function () {
     alert('Redirecting you to book a 15-min call!');
 });
